34c33d61c75b08401ab168cc3da1d985db88dcc3
Raheem14042005 <C22310933@mytudublin.ie>
2026-01-10 20:44:24 +0000

    Update chat.py

--- app/models/chat.py
+++ app/models/chat.py
@@ -1,8 +1,7 @@
 from typing import Any, Dict, List, Optional
 from pydantic import BaseModel
 
+
 class ChatBody(BaseModel):
     message: str
     messages: Optional[List[Dict[str, Any]]] = None
-
-
