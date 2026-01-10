0a1a47c894f60a4ffacbfe118ba6197581e156e3
Raheem14042005 <C22310933@mytudublin.ie>
2026-01-10 21:33:45 +0000

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -7,12 +7,11 @@ from pathlib import Path
 from typing import Any, Dict, List, Optional
 from dotenv import load_dotenv
 
-BASE_DIR = Path(__file__).resolve().parents[2]  # points to project root (where main.py is)
+import boto3
+from fastapi.responses import JSONResponse
 
-# ============================================================
-# BASE DIR (must exist before you build any paths from it)
-# ============================================================
-BASE_DIR = Path(__file__).resolve().parent
+
+BASE_DIR = Path(__file__).resolve().parents[2]  # points to project root (where main.py is)
 
 # ============================================================
 # CONFIG / ENV
